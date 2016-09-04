4c59433f6d2826ddd1fbbdae57727560afe2f3b7
Tom Spencer <fiznool@gmail.com>
2016-09-04 23:25:28 +0100

    Fake service shortening

--- app/components/shortener/shortener.controller.js
+++ app/components/shortener/shortener.controller.js
@@ -3,13 +3,19 @@
 class ShortenerController {
   constructor(service) {
     this.service = service;
-    this.service.sayHello();
   }
 
   shortenUrl(url) {
     console.log('You asked to shorten ' + url);
-    this.currentShink = 'shortened_url';
-    this.currentShinkDestination = url;
+    this.isShorteningUrl = true;
+    this.service.shortenUrl(url)
+      .then(resp => {
+        this.currentShink = resp.data.id;
+        this.currentShinkDestination = resp.data.url;
+      })
+      .finally(() => {
+        this.isShorteningUrl = false;
+      });
   }
 
   clearCurrentShink() {
--- app/components/shortener/shortener.service.js
+++ app/components/shortener/shortener.service.js
@@ -1,9 +1,23 @@
 'use strict';
 
 class ShortenerService {
-  sayHello() {
-    console.log('well, hello');
+  constructor($timeout) {
+    this.$timeout = $timeout;
+  }
+  shortenUrl(url) {
+    console.log('You asked me to shorten a URL!');
+    return this.$timeout(function() {
+      console.log('And, now Im returning!');
+      return {
+        data: {
+          id: '1234',
+          url: url
+        }
+      };
+    }, 1000);
   }
 }
+
+ShortenerService.$inject = ['$timeout'];
 
 module.exports = ShortenerService;
