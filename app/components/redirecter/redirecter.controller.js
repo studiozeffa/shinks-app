e910fe90541bc5a5effff43481f00ef7efe53049
Tom Spencer <fiznool@gmail.com>
2016-09-05 00:23:36 +0100

    Implemented basic redirecter

--- app/components/redirecter/redirecter.controller.js
+++ app/components/redirecter/redirecter.controller.js
@@ -1,11 +1,27 @@
 'use strict';
 
 class RedirecterController {
-  constructor(service) {
+  constructor(service, $stateParams, $window) {
     this.service = service;
+    this.$stateParams = $stateParams;
+    this.$window = $window;
+  }
+
+  $onInit() {
+    this.linkId = this.$stateParams.id;
+    this.service
+      .fetchUrl(this.linkId)
+      .then(resp => {
+        if(!resp || !resp.url) {
+          throw new Error('No URL with the passed link ID was found');
+        }
+
+        this.url = resp.url;
+        this.$window.location.replace(resp.url);
+      });
   }
 }
 
-RedirecterController.$inject = ['RedirecterService'];
+RedirecterController.$inject = ['RedirecterService', '$stateParams', '$window'];
 
 module.exports = RedirecterController;
