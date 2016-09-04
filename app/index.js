736349b84d4cdc7a598f109525baa68254547352
Tom Spencer <fiznool@gmail.com>
2016-09-04 21:22:23 +0100

    Added bulma styling

--- app/index.js
+++ app/index.js
@@ -4,6 +4,8 @@ const angular = require('angular');
 
 const AppComponent = require('./app.component');
 
+require('./app.scss');
+
 const app = angular
   .module('shinks', [])
   .component('app', AppComponent);
