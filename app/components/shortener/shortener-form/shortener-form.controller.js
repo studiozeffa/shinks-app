3d097a2fe8d88d630ca2aebc4d97a3e3cb406400
Tom Spencer <fiznool@gmail.com>
2016-09-04 23:58:00 +0100

    Ensure url is prefixed correctly

--- app/components/shortener/shortener-form/shortener-form.controller.js
+++ app/components/shortener/shortener-form/shortener-form.controller.js
@@ -3,8 +3,17 @@
 class ShortenerFormController {
   onFormSubmit() {
     if(this.model.url) {
-      this.shorten({ url: this.model.url });
+      this.shorten({
+        url: this.ensurePrefix(this.model.url)
+      });
     }
+  }
+
+  ensurePrefix(url) {
+    if(!(/^(?:http|https):\/\/.*/i).test(url)) {
+      url = `http://${url}`;
+    }
+    return url;
   }
 
   $onChanges(changes) {
