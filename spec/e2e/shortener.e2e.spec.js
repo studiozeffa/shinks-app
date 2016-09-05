d99d22a1d09364d13e1213ef80fc45715dbdc876
Tom Spencer <fiznool@gmail.com>
2016-09-05 11:33:58 +0100

    Added basic spec

--- spec/e2e/shortener.e2e.spec.js
+++ spec/e2e/shortener.e2e.spec.js
@@ -1,7 +1,18 @@
 'use strict';
 
 describe('Shortener Page', function() {
-  it('should succeed', function() {
-    expect(1).toBe(1);
+  it('should shorten a link', function() {
+    browser.get('/');
+
+    const form = element(by.css('form[name="urlForm"]'));
+    const urlInput = form.element(by.model('$ctrl.model.url'));
+
+    const results = element(by.css('#shortenerResults'));
+
+    urlInput.clear().sendKeys('http://protractortest.org');
+    form.submit();
+
+    expect(form.isPresent()).toBeFalsy();
+    expect(results.isPresent()).toBeTruthy();
   });
 });
