50f4f9e9e3bbeb4e8f17db33a49ead1f649125d1
Tom Spencer <fiznool@gmail.com>
2016-09-05 11:41:46 +0100

    Moved to page object, added remaining spec blueprints

--- spec/e2e/page-objects/shortener.po.js
+++ spec/e2e/page-objects/shortener.po.js
@@ -6,6 +6,10 @@ class ShortenerPage {
     this.urlInput = this.form.element(by.model('$ctrl.model.url'));
 
     this.results = $('#shortenerResults');
+    this.resultsAnchors = this.results.$$('a');
+    this.shortenedUrlAnchor = this.resultsAnchors.get(0);
+    this.shortenedUrlPreviewAnchor = this.resultsAnchors.get(1);
+    this.destinationUrlAnchor = this.resultsAnchors.get(2);
   }
 
   submitUrl(url) {
--- spec/e2e/shortener.e2e.spec.js
+++ spec/e2e/shortener.e2e.spec.js
@@ -4,11 +4,16 @@ const ShortenerPage = require('./page-objects/shortener.po.js');
 
 describe('Shortener Page', function() {
   const shortenerPage = new ShortenerPage();
+  let originUrl;
 
-  beforeEach(function() {
+  beforeEach(function(done) {
     // Ensure we load the page before each test
     // This ensures we have a fresh slate
     browser.get('/');
+    browser.getCurrentUrl().then(url => {
+      originUrl = url;
+      done();
+    });
   });
 
   it('should shorten a link', function() {
@@ -16,5 +21,14 @@ describe('Shortener Page', function() {
 
     expect(shortenerPage.form.isPresent()).toBeFalsy();
     expect(shortenerPage.results.isPresent()).toBeTruthy();
+
+    expect(shortenerPage.shortenedUrlAnchor.getText()).toMatch(new RegExp(`^${originUrl}[a-zA-Z0-9]+$`));
+    expect(shortenerPage.shortenedUrlPreviewAnchor.getText()).toMatch(new RegExp(`^${originUrl}[a-zA-Z0-9]+\\?preview$`));
+    expect(shortenerPage.destinationUrlAnchor.getText()).toEqual('http://protractortest.org');
   });
+
+  it('should add http:// to the user input when shortening a link if none exists');
+  it('should not submit a shorten request if nothing was filled in to the url box');
+  it('should return to the link shortener when the user selects the Shorten Another button');
+
 });
